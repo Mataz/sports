ba21bb5a7bd0e16d8b1dbf2d3a599665ec10e981
Thomas Mahieux <thomas.mahieux@gmail.com>
2018-04-27 11:09:04 +0200

    Cleaned up rugby.py

--- rugby.py
+++ rugby.py
@@ -8,25 +8,16 @@ driver.get('https://www.flashscore.com/rugby-union/england/aviva-premiership-rug
 
 event_date = driver.find_elements_by_css_selector('td.cell_ad.time')
 dates = [x.text for x in event_date][:10]
-print(dates)        # test
 
 home_team = driver.find_elements_by_css_selector('td.cell_ab.team-home')
 home_teams = [x.text for x in home_team][:10]
-print(home_teams)   # test
 
 away_team = driver.find_elements_by_css_selector('td.cell_ac.team-away')
 away_teams = [x.text for x in away_team][:10]
-print(away_teams)   # test
 
 event_score = driver.find_elements_by_css_selector('td.cell_sa.score.bold')
 scores = [x.text for x in event_score][:10]
-print(scores)       # test
 
 print('Latest scores for the Aviva Premiership Rugby')
 for date, home_team, away_team, score in zip(dates, home_teams, away_teams, scores):
     print(str(date) + ' | ' + str(home_team) + ' - ' + str(away_team) + ' | ' + str(score) + '\n')
-
-
-
-
-
